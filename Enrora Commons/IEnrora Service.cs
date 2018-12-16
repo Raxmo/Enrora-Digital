55ff8f49f9732f45e95ce7fc4a1d3d3404a40272
Raxmo <maxsroe@gmail.com>
2018-12-15 18:08:01 -0700

    [SETUP]

---
+++ Enrora Commons/IEnrora Service.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Enrora_Commons
+{
+	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IEnrora_Service" in both code and config file together.
+	[ServiceContract]
+	public interface IEnrora_Service
+	{
+		[OperationContract]
+		void TEST();
+	}
+}
---
+++ Enrora Server/Enrora Service.cs
@@ -0,0 +1,22 @@
+﻿using Enrora_Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Enrora_Server
+{
+	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Enrora_Service" in both code and config file together.
+	[ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple, InstanceContextMode = InstanceContextMode.Single)]
+	public class Enrora_Service : IEnrora_Service
+	{
+				
+
+		public void TEST()
+		{
+
+		}
+	}
+}
